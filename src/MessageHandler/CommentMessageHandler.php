761c41dd96a60bffef72d5b06830cb22ae914b38
amirKacem <amir.kassem.ben.ammou@gmail.com>
2024-05-05 20:26:01 +0100

    comment spam workflow

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -6,14 +6,20 @@ use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\Service\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 
 final class CommentMessageHandler implements MessageHandlerInterface
 {
     public function __construct(
         private EntityManagerInterface $em,
         private SpamChecker $spamChecker,
-        private CommentRepository $commentRepository
+        private CommentRepository $commentRepository,
+        private WorkflowInterface $commentStateMachine,
+        private LoggerInterface $logger,
+        private MessageBusInterface $bus
     ) {
 
     }
@@ -26,12 +32,23 @@ final class CommentMessageHandler implements MessageHandlerInterface
             return false;
         }
 
-        if(2 === $this->spamChecker->getSpamScore($comment, $message->getContext())) {
-            $comment->setState('spam');
-        } else {
-            $comment->setState('published');
+        if(true === $this->commentStateMachine->can($comment, 'accept')) {
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+            $transition = 'accept';
+            if(2 === $score) {
+                $transition =  'reject_spam';
+            } elseif (1 === $score) {
+                $transition = 'might_be_spam';
+            }
+
+            $this->commentStateMachine->apply($comment, $transition);
+            $this->em->flush();
+
+            $this->bus->dispatch($message);
+
+        } elseif (false === empty($this->logger)) {
+            $this->logger->debug('Dropping comment message', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
         }
 
-        $this->em->flush();
     }
 }
