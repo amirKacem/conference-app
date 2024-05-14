5249628d686aa4fde51ab8c28bc2a8c560581417
amirKacem <amir.kassem.ben.ammou@gmail.com>
2024-05-14 23:20:00 +0100

    upgrade symfony 6

--- src/Command/CommentCleanupCommand.php
+++ src/Command/CommentCleanupCommand.php
@@ -27,7 +27,7 @@ class CommentCleanupCommand extends Command
              ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Dry run');
     }
 
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output):int
     {
         $io = new SymfonyStyle($input, $output);
 
